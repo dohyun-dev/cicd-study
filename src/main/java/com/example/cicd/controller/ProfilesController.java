09da61a88f14161b9bfcf73240533ccac1bedaa5
권도현 <tobeykwon@gmail.com>
2022-12-25 18:03:05 +0900

    fix: ProfilesController requestmapping 수정

--- src/main/java/com/example/cicd/controller/ProfilesController.java
+++ src/main/java/com/example/cicd/controller/ProfilesController.java
@@ -3,10 +3,12 @@ package com.example.cicd.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
+@RequestMapping("/api")
 @RestController
 @RequiredArgsConstructor
 public class ProfilesController {
