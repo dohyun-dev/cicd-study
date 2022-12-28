4d160ff7451018c5e278a8dab0b2e2ac51f1b085
권도현 <tobeykwon@gmail.com>
2022-12-28 21:21:19 +0900

    fix: deploy.sh 변경

--- deploy/server/deploy.sh
+++ deploy/server/deploy.sh
@@ -1,11 +1,21 @@
-# 도커 컨테이너 종료
-docker stop backend-app
+#!/bin/bash
 
-# 도커 컨테이너 삭제
-docker rm backend-app
+# 현재 구동중인 profile 확인
+CURRENT_PROFILE=$(curl -s http://localhost/api/profile)
 
-# 도커 이미지 삭제
-docker rmi backend-app
+echo "> 현재 구동중인 profile: $CURRENT_PROFILE"
+if [ $CURRENT_PROFILE == "set1" ]; then
+  IDLE_PROFILE="set2"
+  IDLE_PORT=8082
+elif [ $CURRENT_PROFILE == "set2" ]; then
+  IDLE_PROFILE="set1"
+  IDLE_PORT=8081
+else
+  echo "> 현재 구동중인 profile을 확인할 수 없습니다. set1 할당"
+  IDLE_PROFILE="set1"
+  IDLE_PORT=8081
+fi
+
 
 # 도커 허브에서 새로운 이미지 pull
 echo "새로운 이미지 pull"
@@ -13,4 +23,11 @@ docker pull tobeykwon/backend-app
 
 # 도커 컨테이너 실행
 echo "도커 컨테이너 실행"
-docker run -itd -p 8080:8080 --name backend-app tobeykwon/backend-app
+docker run -itd -p $IDLE_PORT:8080 -e IDLE_PROFILE=$IDLE_PROFILE --name backend-app-$IDLE_PROFILE tobeykwon/backend-app
+
+# NGINX 포트 스위칭
+sudo sh /home/ubuntu/switch.sh
+
+# 도커 컨테이너 종료 및 삭제
+docker stop backend-app-$CURRENT_PROFILE
+docker rm backend-app-$CURRENT_PROFILE
